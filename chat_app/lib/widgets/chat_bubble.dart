12581cc55c81ece0eaa71df1294110a273809106
leila-digh <leiladabakuyo@cmail.carleton.ca>
2026-02-07 21:17:27 -0500

    commit

---
+++ chat_app/lib/widgets/chat_bubble.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  final String message;
+  final Alignment alignment;
+  const ChatBubble({required this.alignment, required this.message, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: alignment,
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+                '$message',
+                style: TextStyle(fontSize: 20, color: Colors.white)),
+            Image.network('https://cdn-icons-png.flaticon.com/512/6662/6662916.png', height: 200,),
+          ],
+        ),
+        margin: EdgeInsets.all(50),
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+            bottomLeft: Radius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
