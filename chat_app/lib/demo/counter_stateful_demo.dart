6d28c0e0edad3dc1dcaa83c4a9cae88056610dcb
leila-digh <leiladabakuyo@cmail.carleton.ca>
2026-02-09 14:09:45 -0500

    counter app

--- chat_app/lib/demo/counter_stateful_demo.dart
+++ chat_app/lib/demo/counter_stateful_demo.dart
@@ -1,2 +1,58 @@
-
+import 'package:flutter/material.dart';
 //TODO: Recreate the counter app
+class CounterStateful extends StatefulWidget {
+  Color buttonColor;
+
+  CounterStateful({super.key, required this.buttonColor});
+
+  @override
+  State<CounterStateful> createState() {
+    State<CounterStateful> stateClassAssociatedWithThisWidget = _CounterStatefulState();
+    return stateClassAssociatedWithThisWidget;
+  }
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  int counter = 0;
+
+  void increment(){
+    if (mounted) {
+      setState(() {
+        counter ++;
+      });
+    }
+    print(counter);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: widget.buttonColor,
+        child: Icon(Icons.add),
+          onPressed: (){
+            increment();
+          },),
+      body: Center(
+        child: Text(
+          '$counter',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/demo/counter_stateful_demo.dart';
 import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,11 @@ class ChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Chat App",
-      theme: ThemeData(primarySwatch: Colors.yellow),
-      home: const ChatPage(),
+      theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          useMaterial3: false,
+      ),
+      home: CounterStateful(buttonColor: Colors.green,),
     );
   }
 }
