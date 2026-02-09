735d5c8922eb18ea2304d3d766ba71f5ca997cf2
leila-digh <leiladabakuyo@cmail.carleton.ca>
2026-02-09 15:06:40 -0500

    login page

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,42 +1,126 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
+
+  final _formkey = GlobalKey<FormState>();
+  void loginUser() {
+    if (_formkey.currentState!=null && _formkey.currentState!.validate()){
+      print(userNameController.text);
+      print(passwordController.text);
+      print('successful login!');
+    }else{
+      print('unsuccessful login!');
+    }
+
+  }
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5),
-            ),
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5),
+              ),
 
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blueGrey),
-            ),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-            ),
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.blueGrey),
+              ),
+              Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 200,
+              ),
 
-            //TODO: Add Login Button
+              //Username
 
-            //TODO: Add URL Text Button
-          ],
+              Form(
+                key: _formkey,
+                child: Column(
+                    children: [
+                      TextFormField(
+                        validator: (value){
+                          if(value !=null && value.isNotEmpty && value.length < 5){
+                            return "Username should be more than 5 chars";
+                          }else if(value!=null && value.isEmpty){
+                            return "Please type your username";
+                          }
+                          return null;
+                        },
+                        controller: userNameController,
+                        decoration: InputDecoration(
+                          hintText: 'username',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ),
+                      //Password
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'password',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                  onPressed: loginUser,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+                  )),
+              // URL Text button
+              InkWell(
+                splashColor: Colors.red,
+                onDoubleTap: (){
+                  print('Double tap');
+                },
+                onLongPress: (){
+                  print('Long Press');
+                },
+                onTap: (){
+                  // TODO: Navigate to browser
+                  print('Link Clicked');
+                },
+                child: Column(
+                    children: [
+                      Text('Find us on'),
+                      Text('https://pookabhaumik.com'),
+                    ],
+
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
