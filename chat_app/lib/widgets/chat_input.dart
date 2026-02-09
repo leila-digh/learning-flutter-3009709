cc4c0274775905d5b30c023f059609c7b915f775
leila-digh <leiladabakuyo@cmail.carleton.ca>
2026-02-09 18:32:30 -0500

    commit chat_input

---
+++ chat_app/lib/widgets/chat_input.dart
@@ -0,0 +1,63 @@
+import 'package:chat_app/models/chat_message_entity.dart';
+import 'package:flutter/material.dart';
+
+class ChatInput extends StatelessWidget {
+  final Function(ChatMessageEntity) onSubmit;
+  ChatInput({Key? key, required this.onSubmit}) : super(key: key);
+
+  final chatMessageController = TextEditingController();
+
+  void onSendButtonPressed() {
+    print('ChatMessage: ${chatMessageController.text}');
+    
+    final newChatMessage = ChatMessageEntity(
+        text: chatMessageController.text,
+        id: '244',
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        author: Author(userName: 'poojab26'));
+
+    onSubmit(newChatMessage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+          Expanded(
+              child: TextField(
+            keyboardType: TextInputType.multiline,
+            maxLines: 5,
+            minLines: 1,
+            controller: chatMessageController,
+            textCapitalization: TextCapitalization.sentences,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+                hintText: "Type your message",
+                hintStyle: TextStyle(color: Colors.blueGrey),
+                border: InputBorder.none),
+          )),
+          IconButton(
+            onPressed: onSendButtonPressed,
+            icon: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+    );
+  }
+}
